fdc2f41c47a875f8413cf2270a4772921a630f4a
Alba del Rio <adelriocastellvi@gmail.com>
2021-09-08 11:11:24 +0200

    Add Creation form component

--- Cards/frontend/src/components/CreationForm/CreationFrom.tsx
+++ Cards/frontend/src/components/CreationForm/CreationFrom.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View, TextInput, TouchableOpacity, Text
+} from 'react-native';
+import { useDispatch } from 'react-redux';
 
-export default function Carroussel() {
+export default function CreationForm() {
+  const dispatch = useDispatch();
+
+  const [packCardTitle, setPackCardTitle] = useState('');
+  const [packCardImage, setPackCardImage] = useState('');
+  const [packCardSubject, setPackCardSubject] = useState('');
   return (
-    <Text>CreationForm</Text>
+    <View>
+      <View>
+        <TextInput
+          value={packCardTitle}
+          onChangeText={(text:any) => setPackCardTitle(text)}
+          placeholder="Title"
+        />
+
+        <TextInput
+          value={packCardImage}
+          onChangeText={(text:any) => setPackCardImage(text)}
+          placeholder="Image"
+        />
+        <TextInput
+          value={packCardSubject}
+          onChangeText={(text:any) => setPackCardSubject(text)}
+          placeholder="Subject"
+        />
+
+        <TouchableOpacity
+          onPress={() => { }}
+        >
+          <Text>
+            ADD
+
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
