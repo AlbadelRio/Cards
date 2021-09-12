0edaa246bbb66d6709969a356c87d021be051dce
Alba del Rio <adelriocastellvi@gmail.com>
2021-09-12 19:56:14 +0200

    Add styles to updateForm component

---
+++ Cards/frontend/src/components/UpdateForm/updateFormStyles.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  background: {
+    backgroundColor: '#272727',
+    width: '100%'
+  },
+  backgroundCover: {
+    backgroundColor: '#5EBBB0',
+    width: '70%',
+    height: '100%',
+    marginTop: 70,
+    marginLeft: 40
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#EAECF2',
+    margin: 20
+  }
+});
